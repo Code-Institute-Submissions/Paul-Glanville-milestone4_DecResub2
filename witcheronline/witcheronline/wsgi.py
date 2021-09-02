d8e899380fa5cebabd984105ea2e802f8bb3b835
glanville49@googlemail.com <glanville49@googlemail.com>
2021-09-02 19:41:20 +0000

    removed secret key from settings.py

---
+++ witcheronline/witcheronline/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for witcheronline project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'witcheronline.settings')
+
+application = get_wsgi_application()
