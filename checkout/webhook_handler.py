f1ef9df186de7fd14c6af983bbbd05fcc059f30d
glanville49@googlemail.com <glanville49@googlemail.com>
2021-09-25 15:36:08 +0000

    updated stripewh handler with anonymous user for successful payments

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -27,6 +28,21 @@ class stripeWH_handler:
         for field, value in shipping_details.address.item():
             if value == "":
                 shipping_details.address[field] = None
+        
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone,
+                profile.default_country = shipping_details.address.country,
+                profile.default_postcode = shipping_details.address.postal_code,
+                profile.default_town_or_city = shipping_details.address.city,
+                profile.default_street_address1 = shipping_details.address.line1,
+                profile.default_street_address2 = shipping_details.address.line2,
+                profile.default_county = shipping_details.address.county,
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -59,6 +75,7 @@ class stripeWH_handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
