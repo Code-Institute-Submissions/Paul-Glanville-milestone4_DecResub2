e242288cb65d1edaa50f97409512f4818494949b
glanville49@googlemail.com <glanville49@googlemail.com>
2021-09-27 21:36:36 +0000

    fixed error with nav menu not working correctly

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ Credits
 ### Credits
 
 The project is inspired by Boutique Ado mini project and Game online store.
+The Review and wishlist pages were implemented with 'https://github.com/Nazulka/Poster-Panda/tree/master/reviews' as a reference for the models.
 
--- reviews/models.py
+++ reviews/models.py
@@ -4,4 +4,19 @@ from products.models import Product
 from profiles.models import UserProfile
 
 
-class 
+class ItemReviews(models.Model):
+
+    options = [
+        (1, 'poor'),
+        (2, 'average'),
+        (3, 'good'),
+        (4, 'great'),
+        (5, 'excellent')
+    ]
+
+    user =
+    product =
+    header =
+    comment =
+    rating =
+    date_posted =
