c85801830dc6526191c35cc32cef10ee66b11bb0
glanville49@googlemail.com <glanville49@googlemail.com>
2021-09-28 21:15:02 +0000

    updated review options in models.py

--- reviews/models.py
+++ reviews/models.py
@@ -14,9 +14,9 @@ class ItemReviews(models.Model):
         (5, 'excellent')
     ]
 
-    user =
-    product =
-    header =
-    comment =
-    rating =
-    date_posted =
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    header = models.CharField(max_lenght=60, null=False, blank=False)
+    comment = models.CharField(max_lenght=1000, null=False, blank=False)
+    rating = models.IntegerField(choices=options)
+    date_posted = models.DateTimeField(auto_add_now=True)
