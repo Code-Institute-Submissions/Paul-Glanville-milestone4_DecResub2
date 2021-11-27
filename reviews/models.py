30d92e5b0c8e6be3d442438b8200ac95a5bfd094
Paul-Glanville <glanville49@googlemail.com>
2021-11-27 19:27:18 +0000

    corrected spelling mistakes in models.py

--- reviews/models.py
+++ reviews/models.py
@@ -6,7 +6,7 @@ from profiles.models import UserProfile
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    category = models.ManyToManyField(Category, related_name='products')
+    product = models.ManyToManyField(Product, related_name='products')
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
 
@@ -23,10 +23,11 @@ class ItemReviews(models.Model):
 
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    header = models.CharField(max_lenght=60, null=False, blank=False)
-    comment = models.CharField(max_lenght=1000, null=False, blank=False)
+    header = models.CharField(max_length=60, null=False, blank=False)
+    comment = models.CharField(max_length=1000, null=False, blank=False)
     rating = models.IntegerField(choices=options)
-    date_posted = models.DateTimeField(auto_add_now=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.header
