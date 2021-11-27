4287541414d6b10823ace41851313e45f1fc2483
Paul-Glanville <glanville49@googlemail.com>
2021-11-27 10:58:07 +0000

    added env.py file again as was missing

--- witcheronline/settings.py
+++ witcheronline/settings.py
@@ -179,7 +179,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#STATIC_ROOT = '/static/'
 
 if 'USE_AWS' in os.environ:
     # Buucket configuration
