e47ef2cd0e95e772331c943d661bc9c4183886b1
Paul-Glanville <glanville49@googlemail.com>
2021-11-28 22:23:28 +0000

    removed content from views.py in profiles that prevented the profile from loading

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
 
+@login_required
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
 
@@ -12,15 +13,15 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Proflie successfully updated.')
-
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'orders': orders,
         'on_profile_page': True
     }
 
