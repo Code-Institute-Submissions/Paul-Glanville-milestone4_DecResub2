be005d3e2df9e0a5672cbff6358ff9e342b3dbe6
Paul-Glanville <glanville49@googlemail.com>
2021-11-29 07:36:51 +0000

    added cache control in settings.py

--- witcheronline/settings.py
+++ witcheronline/settings.py
@@ -182,7 +182,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
-    # Buucket configuration
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # Bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'witcheronline'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
