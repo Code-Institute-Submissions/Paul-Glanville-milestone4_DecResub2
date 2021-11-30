9708966f9f1a0c8f881f6c52e71302b0d89f8093
Paul-Glanville <glanville49@googlemail.com>
2021-11-30 08:08:25 +0000

    added contenet to wishlist models and changed debug to false

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,24 +1,26 @@
 from django.db import models
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from products.models import Product
 from profiles.models import UserProfile
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    product = models.ManyToManyField(Product, related_name='products')
-    created = models.DateField(auto_now_add=True)
-    updated = models.DateField(auto_now=True)
+class wishlist(models.Model):
+    """ Model for maintaining a wishlist """
 
-
-class ItemWishlist(models.Model):
-
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    header = models.CharField(max_length=60, null=False, blank=False)
-    comment = models.CharField(max_length=1000, null=False, blank=False)
-    rating = models.IntegerField(choices=options)
-    date_posted = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(UserProfile, null=False, blank=False,
+                             on_delete=models.CASCADE,
+                             related_name='wishlist')
+    products = models.ManyToManyField(Product, through='WishlistItem')
 
     def __str__(self):
-        return self.header
+        return f'Wishlist ({self.user})'
+
+
+@receiver(post_save, sender=UserProfile)
+def create_or_update_user_wishlist(sender, instance, created, **kwargs):
+    """
+    Create wishlist when user signs up
+    """
+    if created:
+        Wishlist.objects.create(user=instance)
